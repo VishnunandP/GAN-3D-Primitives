9038423d280de22fbd89ae7368288b99466c28f5
VishnunandP <vishnunand.p@gmail.com>
2025-04-29 22:46:18 +0530

    Update gan_model.py

--- models/gan_model.py
+++ models/gan_model.py
@@ -1,1 +1,37 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(100, 256),  
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 3)  
+        )
+
+    def forward(self, z):
+        return self.fc(z)
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(3, 512),  
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),  
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.fc(x)
 
